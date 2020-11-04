cc3e0f614ee99ac4311c1f3430daaaf7519b3fe6
Scott Allen <scott-allen@lambdastudents.com>
2020-11-04 08:38:55 -0700

    connected state to the app component

--- src/reducers/carReducer.js
+++ src/reducers/carReducer.js
@@ -14,7 +14,7 @@ const initialState = {
         { id: 2, name: 'Racing detail package', price: 1500 },
         { id: 3, name: 'Premium sound system', price: 500 },
         { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
+    ],
 };
 
 export const carReducer = (state = initialState, action) => {
